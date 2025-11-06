a8d2518618b7674b7c8e7f2202c8bcc6b1a531d0
Ellheim <i.dmitriy.kravchuk@gmail.com>
2025-11-06 12:40:01 +0100

    Add setup.py and MANIFEST.in for pip installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inventree-custom-stock-plugin',
+    version='1.0',
+    description='Custom Stock View Plugin for InvenTree',
+    author='Ellheim',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
